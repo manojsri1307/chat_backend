30294d5d37034b4a7d9150788e9bf1b1fbf8249a
manojsri1307 <manojsri2002@gmail.com>
2025-06-01 19:13:37 +0530

    Fix: Use dynamic port for Render deployment

--- NewBack.js
+++ NewBack.js
@@ -45,6 +45,7 @@ io.on("connection", (socket) => {
     }
   });
 });
-server.listen(5000, () => {
-  console.log(":rocket: Server is running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
